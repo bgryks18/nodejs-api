a09c9b6fad64830d4d2c229fcb95bc93da05ea62
Bugra Yuksel <bgryks18@gmail.com>
2022-03-12 19:31:35 +0300

    veritabanı config bilgileri girildi

---
+++ config/index.js
@@ -0,0 +1,12 @@
+/** @format */
+
+require("dotenv").config();
+const mysql = require("mysql2");
+const pool = mysql.createPool({
+	host: process.env.HOST,
+	user: process.env.USER,
+	database: process.env.DATABASE,
+	password: process.env.PASSWORD,
+});
+
+module.exports = pool;
